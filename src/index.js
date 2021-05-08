f27f1ac1b9ce5b0d67f80ad9b6c55655e89f480c
hemanth253 <83533561+hemanth253@users.noreply.github.com>
2021-05-08 21:07:46 +0530

    v1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+//  tag a requires closing tags
+//  pass js object to the style using {}
+//  camelCasing of keys of js objects
+//  values of js object shoud be string
+
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+
+//  use of inline styling - can convert in 1 go.
+customStyle.color = "blue";
+
+ReactDOM.render(
+  <h1 style={customStyle}>Hello World!</h1>,
+  document.getElementById("root")
+);
